23f8af0a1f5184831d8a454f9d148326c391c621
Hugo Viza <hugo.viza@hms.mx>
2021-01-12 01:21:35 -0600

    version 1 app

--- Arkasis_API/Controllers/SolicitudDispersionController.cs
+++ Arkasis_API/Controllers/SolicitudDispersionController.cs
@@ -46,7 +46,7 @@ namespace Arkasis_API.Controllers
             {
                 if (arrayResult[0].Rows.Count > 0)
                 {
-                    return Ok(new { Mensaje = "Guardado correctamente", Success = false });
+                    return Ok(new { Mensaje = "Guardado correctamente", Success = true, Resultado = "1" });
                 }
                 else
                 {
@@ -161,6 +161,20 @@ namespace Arkasis_API.Controllers
 
             if (sd.IdCliente == "")
             {
+
+                /*queries.Add($@"SET @idCliente = (select RIGHT('000000' + CAST( (max(cteLlave) + 1) AS VARCHAR), 7) as consecutivo from arcicte)");
+                queries.Add($@"SET @idGrupo = (select RIGHT('000000' + CAST( (max(gruLlave) + 1) AS VARCHAR), 7) as consecutivo from arcigru)");
+
+                queries.Add($@"insert into arcigru 
+                                (gruLlave, gruX001, gruX003, gruX004, gruX005, gruX005c, gruX007, gruX014, gruX015, gruX016, gruX016c, gruX301, gruX302, gruX303, gruX304) values
+                                (@idGrupo, '{sd.IdSucursal}', '{sd.StrNombreCompleto}', '1', '{sd.idPromotor}', '{sd.StrPromotor}', '{sd.StrFechaAlta}', '{sd.IdDomicilioEstado}', '{sd.IdDomicilioMunicipio}', '1', 'INDIVIDUAL', '{sd.StrUsuarioPromotor}', GETDATE(), '{sd.StrUsuarioPromotor}', GETDATE())");
+
+
+                queries.Add($@"insert into arcicte 
+                                (cteLlave, cteX001, cteX003, cteX004, cteX005, cteX006, cteX007, cteX008, cteX009, cteX010, cteX012, cteX013, cteX014, cteX015, cteX016, cteX019, cteX020, cteX021, cteX023, cteX024, cteX025, cteX026, cteX027, cteX028, cteX030, cteX031, cteX031c, cteX033, cteX034, cteX036, cteX037, cteX040, cteX046, cteX047, cteX048, cteX049, cteX041, cteX301, cteX302, cteX303, cteX304 ) values
+                                (@idCliente, '{sd.IdSucursal}', '{sd.StrApellidoPaterno}', '{sd.StrApellidoMaterno}', '{sd.StrNombre1}', '{sd.StrNombre2}', '{sd.StrNombreCompleto}', '{sd.StrDomicilio}', '{sd.StrDomicilioNumExt}', '{sd.StrDomicilioNumInt}', '{sd.StrDomicilioColonia}', '{sd.IdDomicilioEstado}', '{sd.StrDomicilioEstado}', '{sd.IdDomicilioMunicipio}', '{sd.StrDomicilioMunicipio}', '{sd.StrDomicilioCodigoPostal}', '{sd.StrTelefono}', '{sd.StrCelular}', '{sd.StrCURP}', '{sd.StrPais}', '{sd.StrEstadoNacimiento}', '{sd.StrNacionalidad}', '{sd.IdGenero}', '{sd.StrGenero}', '{sd.StrFechaNacimiento}', '{sd.IdEstadoCivil}', '{sd.StrEstadoCivil}', '{sd.StrNumeroINE}', '{sd.StrClaveINE}', '{sd.StrEmail}', '{sd.StrOcupacion}', '{sd.StrActividad}', '{sd.StrNombreConyuge}', '{sd.StrFechaNacimientoConyuge}', '{sd.StrLugarNacimientoConyuge}', '{sd.StrOcupacionConyuge}', @idGrupo, '{sd.StrUsuarioPromotor}', GETDATE(), '{sd.StrUsuarioPromotor}', GETDATE())");
+                */
+
                 queries.Add($@"insert into arcigru 
                                 (gruX001, gruX003, gruX004, gruX005, gruX005c, gruX007, gruX014, gruX015, gruX016, gruX016c, gruX301, gruX302, gruX303, gruX304) values
                                 ('{sd.IdSucursal}', '{sd.StrNombreCompleto}', '1', '{sd.idPromotor}', '{sd.StrPromotor}', '{sd.StrFechaAlta}', '{sd.IdDomicilioEstado}', '{sd.IdDomicilioMunicipio}', '1', 'INDIVIDUAL', '{sd.StrUsuarioPromotor}', GETDATE(), '{sd.StrUsuarioPromotor}', GETDATE())");
@@ -179,8 +193,8 @@ namespace Arkasis_API.Controllers
             }
 
             queries.Add($@"insert into arcigrm
-                                (grmX001, grmX002, grmX003, grmX004, grmX005, grmX006, grmX010, grmX012, grmX018, grmX019, grmX025, grmX036, grmX301, grmX302, grmX303, grmX304 ) values
-                                ('{sd.IdSucursal}', @idGrupo, '{sd.StrNombreCompleto}' , '1', '{sd.idPromotor}', '{sd.StrPromotor}', '{sd.DblMontoAutorizado}', '{sd.IntPlazo}', '1', '1', '{sd.StrFechaAlta}', '{sd.DblMontoSolicitado}', '{sd.StrUsuarioPromotor}', GETDATE(), '{sd.StrUsuarioPromotor}', GETDATE())");
+                                (grmX001, grmX002, grmX003, grmX004, grmX005, grmX006, grmX010, grmX012, grmX018, grmX019, grmX025, grmX036, grmX301, grmX302, grmX303, grmX304, grmX020, grmX021, grmX026 ) values
+                                ('{sd.IdSucursal}', @idGrupo, '{sd.StrNombreCompleto}' , '1', '{sd.idPromotor}', '{sd.StrPromotor}', '{sd.DblMontoAutorizado}', '{sd.IntPlazo}', '1', '1', '{sd.StrFechaAlta}', '{sd.DblMontoSolicitado}', '{sd.StrUsuarioPromotor}', GETDATE(), '{sd.StrUsuarioPromotor}', GETDATE(), 'INDIVIDUAL', '{sd.StrFechaAlta}', '{sd.StrFechaAlta}')");
 
             queries.Add($@"insert into arciced
                                 (solX001, solX003, solX004, solX005, solX006, cedLlave, cedX003, cedX004, cedX005, cedX006, cedX007, cedX008, cedX009, cedX010, cedX012, cedX013, cedX014, cedX015, cedX016, cedX019, cedX020, cedX021, cedX023, cedX024, cedX025, cedX026, cedX027, cedX028, cedX030, cedX031, cedX033, cedX034, cedX036, cedX037, cedX040, cedX046, cedX047, cedX048, cedX049, cedX054c, cedX054d, cedX301, cedX302, cedX303, cedX304) values
